ed36131085055ecdf90bc8ebe3f237c9afb154aa
rkudashev <kudashev.ravil@yandex.ru>
2024-06-06 17:53:47 +0500

    Добавил админскую сущность.

---
+++ TaskManager.API/Models/ProjectAdmin.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskManager.API.Models
+{
+    public class ProjectAdmin
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public List<Project> Projects { get; set; } = new List<Project>();
+        public ProjectAdmin(User user)
+        {
+            UserId = user.Id;
+            User = user;
+        }
+    }
+}
